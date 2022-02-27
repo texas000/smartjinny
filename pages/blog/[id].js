322fdc3c0eafc9631cce9652f0b5203f3b31970f
SMARTJIN-MACBOOK <texas000@hotmail.com>
2022-02-27 00:18:28 -0500

    adding push animation

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -17,6 +17,7 @@ export default function Post() {
 	);
 	const [post, setPost] = useState(EditorState.createEmpty());
 	const [title, setTitle] = useState(false);
+	const [isLoading, setLoading] = useState(false);
 
 	const raw = Cookies.get("user_session");
 	const token = jwt.decode(raw);
@@ -77,8 +78,9 @@ export default function Post() {
 								{token && (
 									<div className="my-2">
 										<button
-											className="font-semibold rounded-md bg-indigo-200 hover:bg-indigo-300 text-indigo-700 px-5 py-2 mr-2"
+											className="inline-flex font-semibold rounded-md bg-indigo-200 hover:bg-indigo-300 text-indigo-700 px-5 py-2 mr-2"
 											onClick={() => {
+												setLoading(true);
 												fetch("/api/blog/updatePost", {
 													method: "POST",
 													body: JSON.stringify({
@@ -88,10 +90,32 @@ export default function Post() {
 															convertToRaw(post.getCurrentContent())
 														),
 													}),
-												});
+												}).then(() => setLoading(false));
 											}}
 										>
-											Push
+											{isLoading && (
+												<svg
+													class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+													xmlns="http://www.w3.org/2000/svg"
+													fill="none"
+													viewBox="0 0 24 24"
+												>
+													<circle
+														class="opacity-25"
+														cx="12"
+														cy="12"
+														r="10"
+														stroke="currentColor"
+														stroke-width="4"
+													></circle>
+													<path
+														class="opacity-75"
+														fill="currentColor"
+														d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+													></path>
+												</svg>
+											)}
+											<span>Push</span>
 										</button>
 										<button
 											className="font-semibold rounded-md bg-red-200 hover:bg-red-300 text-red-700 px-5 py-2"
