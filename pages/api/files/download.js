addb14a674ee138e6959fa401adeb55b1e8a1f0e
SMARTJIN <texas000@hotmail.com>
2022-04-13 20:52:17 -0400

    adding files ui

--- pages/api/files/download.js
+++ pages/api/files/download.js
@@ -1,14 +1,14 @@
 export default async (req, res) => {
 	var status;
 	const dropbox = await fetch(
-		"https://api.dropboxapi.com/2/files/list_folder",
+		"https://api.dropboxapi.com/2/files/get_temporary_link",
 		{
 			method: "POST",
 			headers: {
 				Authorization: `Bearer ${process.env.DROPBOX_API_KEY}`,
 				"Content-Type": "application/json",
 			},
-			body: JSON.stringify({ path: "" }),
+			body: req.body,
 		}
 	)
 		.then(async (res) => {
