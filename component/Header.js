bc72505a38334fa7b72d458986fe8e0d4c7c0164
SMARTJIN-MACBOOK <texas000@hotmail.com>
2022-05-31 20:38:59 -0400

    adding headers

--- component/Header.js
+++ component/Header.js
@@ -1,22 +1,244 @@
+import { styled, alpha } from "@mui/material/styles";
+import SearchIcon from "@mui/icons-material/Search";
+import MenuIcon from "@mui/icons-material/Menu";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import HomeIcon from "@mui/icons-material/Home";
+import {
+	AppBar,
+	Avatar,
+	Box,
+	Container,
+	IconButton,
+	Toolbar,
+	Tooltip,
+	Typography,
+	InputBase,
+	Menu,
+	MenuItem,
+	Divider,
+	List,
+	ListItem,
+	ListItemButton,
+	ListItemIcon,
+	ListItemText,
+	Drawer,
+} from "@mui/material";
+import { useState } from "react";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+
+const Search = styled("div")(({ theme }) => ({
+	position: "relative",
+	borderRadius: theme.shape.borderRadius,
+	backgroundColor: alpha(theme.palette.common.white, 0.15),
+	"&:hover": {
+		backgroundColor: alpha(theme.palette.common.white, 0.25),
+	},
+	marginLeft: 0,
+	width: "100%",
+	[theme.breakpoints.up("sm")]: {
+		marginLeft: theme.spacing(1),
+		width: "auto",
+	},
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+	padding: theme.spacing(0, 2),
+	height: "100%",
+	position: "absolute",
+	pointerEvents: "none",
+	display: "flex",
+	alignItems: "center",
+	justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+	color: "inherit",
+	"& .MuiInputBase-input": {
+		padding: theme.spacing(1, 1, 1, 0),
+		// vertical padding + font size from searchIcon
+		paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+		transition: theme.transitions.create("width"),
+		width: "100%",
+		[theme.breakpoints.up("sm")]: {
+			width: "12ch",
+			"&:focus": {
+				width: "20ch",
+			},
+		},
+	},
+}));
+
 export default function Header() {
+	const [anchorEl, setAnchorEl] = useState(null);
+	const [sideMenu, setSideMenu] = useState(false);
+
+	const toggleDrawer = (open) => (event) => {
+		if (
+			event.type === "keydown" &&
+			(event.key === "Tab" || event.key === "Shift")
+		) {
+			return;
+		}
+
+		setSideMenu(open);
+	};
+
+	const list = (anchor) => (
+		<Box
+			sx={{ width: 250 }}
+			role="presentation"
+			onClick={toggleDrawer(false)}
+			onKeyDown={toggleDrawer(false)}
+		>
+			<List>
+				{["HOME"].map((text, index) => (
+					<ListItem key={text} disablePadding>
+						<ListItemButton href="/">
+							<ListItemIcon>
+								<HomeIcon />
+							</ListItemIcon>
+							<ListItemText primary={text} />
+						</ListItemButton>
+					</ListItem>
+				))}
+			</List>
+			<Divider />
+			<List>
+				{["blog", "contact", "sign", "file"].map((text, index) => (
+					<ListItem key={text} disablePadding>
+						<ListItemButton href={`/${text}`}>
+							<ListItemIcon>
+								{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+							</ListItemIcon>
+							<ListItemText primary={text} className="uppercase" />
+						</ListItemButton>
+					</ListItem>
+				))}
+			</List>
+		</Box>
+	);
+
+	const isMenuOpen = Boolean(anchorEl);
+	const handleProfileMenuOpen = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+
+	const handleMenuClose = () => {
+		setAnchorEl(null);
+	};
+
+	const menuId = "primary-search-account-menu";
+	const renderMenu = (
+		<Menu
+			anchorEl={anchorEl}
+			anchorOrigin={{
+				vertical: "top",
+				horizontal: "right",
+			}}
+			id={menuId}
+			keepMounted
+			transformOrigin={{
+				vertical: "top",
+				horizontal: "right",
+			}}
+			open={isMenuOpen}
+			onClose={handleMenuClose}
+		>
+			<MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+			<MenuItem onClick={handleMenuClose}>My account</MenuItem>
+		</Menu>
+	);
+
 	return (
-		<div className="ud-header sticky bg-transparent top-0 left-0 z-40 w-full flex items-center">
-			<div className="container">
-				<div className="flex -mx-4 items-center justify-between relative">
-					<div className="px-4 w-60 max-w-full">
-						<a href="/" className="navbar-logo w-full block py-5">
-							<img
-								src="/assets/images/logo/logo.svg"
-								alt="logo"
-								className="w-full header-logo"
-							/>
-						</a>
-					</div>
-					<div className="flex px-4 justify-between items-center w-full">
-						<div>
-							<button
-								id="navbarToggler"
-								className="
+		<>
+			<Box sx={{ flexGrow: 1 }}>
+				<AppBar position="static">
+					<Container maxWidth="xl">
+						<Toolbar disableGutters>
+							<IconButton
+								size="large"
+								edge="start"
+								color="inherit"
+								aria-label="open drawer"
+								sx={{ mr: 2 }}
+								onClick={toggleDrawer(true)}
+							>
+								<MenuIcon />
+							</IconButton>
+							<Typography
+								variant="h6"
+								noWrap
+								component="a"
+								href="/"
+								sx={{
+									mr: 2,
+									display: { xs: "none", md: "flex" },
+									fontFamily: "monospace",
+									fontWeight: 700,
+									letterSpacing: ".3rem",
+									color: "inherit",
+									textDecoration: "none",
+								}}
+							>
+								<img
+									src="/assets/images/logo/logo.svg"
+									alt="logo"
+									className="w-full header-logo"
+								/>
+							</Typography>
+
+							<Search>
+								<SearchIconWrapper>
+									<SearchIcon />
+								</SearchIconWrapper>
+								<StyledInputBase
+									placeholder="Search…"
+									inputProps={{ "aria-label": "search" }}
+								/>
+							</Search>
+
+							<Box sx={{ flexGrow: 0 }}>
+								<Tooltip title="Open settings">
+									<IconButton
+										size="large"
+										edge="end"
+										aria-label="account of current user"
+										aria-controls={menuId}
+										aria-haspopup="true"
+										onClick={handleProfileMenuOpen}
+										color="inherit"
+									>
+										<AccountCircle />
+									</IconButton>
+								</Tooltip>
+							</Box>
+						</Toolbar>
+					</Container>
+				</AppBar>
+				{renderMenu}
+			</Box>
+
+			<Drawer anchor="left" open={sideMenu} onClose={toggleDrawer(false)}>
+				{list()}
+			</Drawer>
+			{/* <div className="ud-header sticky bg-transparent top-0 left-0 z-40 w-full flex items-center">
+				<div className="container">
+					<div className="flex -mx-4 items-center justify-between relative">
+						<div className="px-4 w-60 max-w-full">
+							<a href="/" className="navbar-logo w-full block py-5">
+								<img
+									src="/assets/images/logo/logo.svg"
+									alt="logo"
+									className="w-full header-logo"
+								/>
+							</a>
+						</div>
+						<div className="flex px-4 justify-between items-center w-full">
+							<div>
+								<button
+									id="navbarToggler"
+									className="
                   block
                   absolute
                   right-4
@@ -29,14 +251,14 @@ export default function Header() {
                   rounded-lg
 				  lg:hidden
                 "
-							>
-								<span className="relative w-[30px] h-[2px] my-[6px] block bg-white"></span>
-								<span className="relative w-[30px] h-[2px] my-[6px] block bg-white"></span>
-								<span className="relative w-[30px] h-[2px] my-[6px] block bg-white"></span>
-							</button>
-							<nav
-								id="navbarCollapse"
-								className="
+								>
+									<span className="relative w-[30px] h-[2px] my-[6px] block bg-white"></span>
+									<span className="relative w-[30px] h-[2px] my-[6px] block bg-white"></span>
+									<span className="relative w-[30px] h-[2px] my-[6px] block bg-white"></span>
+								</button>
+								<nav
+									id="navbarCollapse"
+									className="
                   absolute
                   py-5
                   lg:py-0 lg:px-4
@@ -53,12 +275,12 @@ export default function Header() {
                   hidden
                   lg:block lg:static lg:shadow-none
                 "
-							>
-								<ul className="blcok lg:flex">
-									<li className="relative group">
-										<a
-											href="#home"
-											className="
+								>
+									<ul className="blcok lg:flex">
+										<li className="relative group">
+											<a
+												href="#home"
+												className="
                         ud-menu-scroll
                         text-base text-dark
                         lg:text-white
@@ -71,14 +293,14 @@ export default function Header() {
                         mx-8
                         lg:mr-0
                       "
-										>
-											Home
-										</a>
-									</li>
-									<li className="relative group">
-										<a
-											href="#about"
-											className="
+											>
+												Home
+											</a>
+										</li>
+										<li className="relative group">
+											<a
+												href="#about"
+												className="
                         ud-menu-scroll
                         text-base text-dark
                         lg:text-white
@@ -92,14 +314,14 @@ export default function Header() {
                         lg:mr-0 lg:ml-7
                         xl:ml-12
                       "
-										>
-											About
-										</a>
-									</li>
-									<li className="relative group">
-										<a
-											href="#pricing"
-											className="
+											>
+												About
+											</a>
+										</li>
+										<li className="relative group">
+											<a
+												href="#pricing"
+												className="
                         ud-menu-scroll
                         text-base text-dark
                         lg:text-white
@@ -113,14 +335,14 @@ export default function Header() {
                         lg:mr-0 lg:ml-7
                         xl:ml-12
                       "
-										>
-											Pricing
-										</a>
-									</li>
-									<li className="relative group">
-										<a
-											href="#team"
-											className="
+											>
+												Pricing
+											</a>
+										</li>
+										<li className="relative group">
+											<a
+												href="#team"
+												className="
                         ud-menu-scroll
                         text-base text-dark
                         lg:text-white
@@ -134,14 +356,14 @@ export default function Header() {
                         lg:mr-0 lg:ml-7
                         xl:ml-12
                       "
-										>
-											Team
-										</a>
-									</li>
-									<li className="relative group">
-										<a
-											href="#contact"
-											className="
+											>
+												Team
+											</a>
+										</li>
+										<li className="relative group">
+											<a
+												href="#contact"
+												className="
                         ud-menu-scroll
                         text-base text-dark
                         lg:text-white
@@ -155,14 +377,14 @@ export default function Header() {
                         lg:mr-0 lg:ml-7
                         xl:ml-12
                       "
-										>
-											Contact
-										</a>
-									</li>
-									<li className="relative group submenu-item">
-										<a
-											href="#"
-											className="
+											>
+												Contact
+											</a>
+										</li>
+										<li className="relative group submenu-item">
+											<a
+												href="#"
+												className="
                         text-base text-dark
                         lg:text-white
                         lg:group-hover:opacity-70
@@ -188,11 +410,11 @@ export default function Header() {
                         after:-translate-y-1/2
                         after:mt-[-2px]
                       "
-										>
-											Pages
-										</a>
-										<div
-											className="
+											>
+												Pages
+											</a>
+											<div
+												className="
                         submenu
                         hidden
                         relative
@@ -211,10 +433,10 @@ export default function Header() {
                         transition-[top]
                         duration-300
                       "
-										>
-											<a
-												href="about.html"
-												className="
+											>
+												<a
+													href="about.html"
+													className="
                           block
                           text-sm text-body-color
                           rounded
@@ -222,12 +444,12 @@ export default function Header() {
                           py-[10px]
                           px-4
                         "
-											>
-												About Page
-											</a>
-											<a
-												href="pricing.html"
-												className="
+												>
+													About Page
+												</a>
+												<a
+													href="pricing.html"
+													className="
                           block
                           text-sm text-body-color
                           rounded
@@ -235,12 +457,12 @@ export default function Header() {
                           py-[10px]
                           px-4
                         "
-											>
-												Pricing Page
-											</a>
-											<a
-												href="contact.html"
-												className="
+												>
+													Pricing Page
+												</a>
+												<a
+													href="contact.html"
+													className="
                           block
                           text-sm text-body-color
                           rounded
@@ -248,12 +470,12 @@ export default function Header() {
                           py-[10px]
                           px-4
                         "
-											>
-												Contact Page
-											</a>
-											<a
-												href="blog-grids.html"
-												className="
+												>
+													Contact Page
+												</a>
+												<a
+													href="blog-grids.html"
+													className="
                           block
                           text-sm text-body-color
                           rounded
@@ -261,12 +483,12 @@ export default function Header() {
                           py-[10px]
                           px-4
                         "
-											>
-												Blog Grid Page
-											</a>
-											<a
-												href="blog-details.html"
-												className="
+												>
+													Blog Grid Page
+												</a>
+												<a
+													href="blog-details.html"
+													className="
                           block
                           text-sm text-body-color
                           rounded
@@ -274,12 +496,12 @@ export default function Header() {
                           py-[10px]
                           px-4
                         "
-											>
-												Blog Details Page
-											</a>
-											<a
-												href="signup.html"
-												className="
+												>
+													Blog Details Page
+												</a>
+												<a
+													href="signup.html"
+													className="
                           block
                           text-sm text-body-color
                           rounded
@@ -287,12 +509,12 @@ export default function Header() {
                           py-[10px]
                           px-4
                         "
-											>
-												Sign Up Page
-											</a>
-											<a
-												href="signin.html"
-												className="
+												>
+													Sign Up Page
+												</a>
+												<a
+													href="signin.html"
+													className="
                           block
                           text-sm text-body-color
                           rounded
@@ -300,12 +522,12 @@ export default function Header() {
                           py-[10px]
                           px-4
                         "
-											>
-												Sign In Page
-											</a>
-											<a
-												href="404.html"
-												className="
+												>
+													Sign In Page
+												</a>
+												<a
+													href="404.html"
+													className="
                           block
                           text-sm text-body-color
                           rounded
@@ -313,18 +535,18 @@ export default function Header() {
                           py-[10px]
                           px-4
                         "
-											>
-												404 Page
-											</a>
-										</div>
-									</li>
-								</ul>
-							</nav>
-						</div>
-						<div className="sm:flex justify-end pr-16 lg:pr-0 my-1">
-							<a
-								href="/blog"
-								className="
+												>
+													404 Page
+												</a>
+											</div>
+										</li>
+									</ul>
+								</nav>
+							</div>
+							<div className="sm:flex justify-end pr-16 lg:pr-0 my-1">
+								<a
+									href="/blog"
+									className="
                   text-base
                   font-medium
                   text-white
@@ -338,13 +560,14 @@ export default function Header() {
                   duration-300
                   ease-in-out
                 "
-							>
-								Blog
-							</a>
+								>
+									Blog
+								</a>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		</div>
+			</div> */}
+		</>
 	);
 }
