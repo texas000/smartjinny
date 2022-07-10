98efa8fdf4b24bfdd19bd65cb72742c324bdbcbe
SMARTJIN-MACBOOK <texas000@hotmail.com>
2022-07-10 17:17:12 -0400

    gtm problem solve

--- component/Page.js
+++ component/Page.js
@@ -14,16 +14,6 @@ export default function Page({ children, title, description }) {
 						"Smartjinny, Professional Software Developer Portfolio Website"
 					}
 				></meta>
-
-				<Script id="google-tag-manager" strategy="afterInteractive">
-					{`
-	(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-	})(window,document,'script','dataLayer','GTM-T5KW8HH');
-	`}
-				</Script>
 				<meta property="og:site_name" content="SMARTJINNY" />
 				<meta property="og:type" content="article" />
 				<meta property="og:title" content="SMARTJINNY" />
@@ -43,10 +33,15 @@ export default function Page({ children, title, description }) {
 				></meta>
 				<link rel="icon" href="/assets/images/favicon.png" />
 			</Head>
-
-			<Header />
-			{children}
-
+			<Script id="google-tag-manager" strategy="afterInteractive">
+				{`
+	(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-T5KW8HH');
+	`}
+			</Script>
 			<noscript>
 				<iframe
 					src="https://www.googletagmanager.com/ns.html?id=GTM-T5KW8HH"
@@ -55,6 +50,9 @@ export default function Page({ children, title, description }) {
 					style={{ display: "none", visibility: "hidden" }}
 				></iframe>
 			</noscript>
+
+			<Header />
+			{children}
 		</Fragment>
 	);
 }
