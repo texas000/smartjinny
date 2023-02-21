ba5617a3707f32e1523776b5ae1932ffedf711c5
texas000 <texas000@hotmail.com>
2023-02-21 00:41:53 -0500

    connected to cms

---
+++ app/blog/[id]/page.js
@@ -0,0 +1,38 @@
+// import { NextSeo } from 'next-seo';
+import { Fragment } from "react";
+
+async function blogContent(slug) {
+    const res = await fetch(`https://api.smartjinny.com/blog/content?slug=${slug}`);
+    // The return value is *not* serialized
+    // You can return Date, Map, Set, etc.
+  
+    // Recommendation: handle errors
+    if (!res.ok) {
+      // This will activate the closest `error.js` Error Boundary
+      throw new Error('Failed to fetch data');
+    }    
+    
+  return res.json();
+}
+
+export default async function Page({ params, searchParams }) {
+    const data = await blogContent(params.id);
+    return (
+      <div>
+        <title>{data.data?.head?.title || "Not Found"}</title>
+        <meta content={data.data?.head?.description || "Not Found"} name="description"></meta>
+        <meta content={data.data?.head?.title || "Not Found"} property="og:title" />
+        <meta content={data.data?.head?.description || "Not Found"} property="og:description" />
+        <meta content={data.data?.head?.title || "Not Found"} property="twitter:title" />
+        <meta content={data.data?.head?.description || "Not Found"} property="twitter:description" />
+        <meta property="og:type" content="website" />
+        <meta content="summary_large_image" name="twitter:card" />
+	    <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta content="Webflow" name="generator" />
+
+        <div dangerouslySetInnerHTML={{ __html: data.data?.body }}/>
+      </div>
+    );
+}
+// <div>{JSON.stringify(data)}</div>
+// <p>ID: {params.id}</p>
--- pagess/api/agent/listAgent.js
+++
@@ -1,9 +0,0 @@
-import { connectToDatabase } from "../../../mongo";
-
-export default async (req, res) => {
-	const { db } = await connectToDatabase();
-
-	const post = await db.collection("agent").find().toArray();
-
-	res.json(post);
-};
