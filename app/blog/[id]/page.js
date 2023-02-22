6bbee39ec5755e5f00c4ecf80186e7ffc25e26fd
texas000 <texas000@hotmail.com>
2023-02-21 19:03:07 -0500

    adding exception

--- app/blog/[id]/page.js
+++ app/blog/[id]/page.js
@@ -8,34 +8,40 @@ async function blogContent(path) {
 
 export default async function Page({ params, searchParams }) {
   const data = await blogContent(params.id);
-  return (
-    <div className="m-4">
-      
-        <title>{data.title || "Not Found"}</title>
-        <meta
-          content={data.description || "Not Found"}
-          name="description"
-        ></meta>
-        <meta content={data.title || "Not Found"} property="og:title" />
-        <meta
-          content={data.description || "Not Found"}
-          property="og:description"
-        />
-        <meta content={data.title || "Not Found"} property="twitter:title" />
-        <meta
-          content={data.description || "Not Found"}
-          property="twitter:description"
-        />
-        <meta property="og:type" content="website" />
-        <meta content="summary_large_image" name="twitter:card" />
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <meta content="Smartjinny" name="generator" />
-      <h1 className="text-center content-center font-extrabold text-6xl">
-        {data.title}
-      </h1>
-      <div className="xl:mx-40 lg:mx-30 sm:mx-20 xs:mx-5 mt-10 prose md:prose-lg lg:prose-xl prose-img:rounded-xl prose-img:max-w-lg prose-a:text-blue-600 hover:prose-a:text-blue-500 max-w-none">
-        <div dangerouslySetInnerHTML={{ __html: data.html }}/>
+  if(params.id && data) {
+    return (
+      <div className="m-4">
+          <title>{data.title || "Not Found"}</title>
+          <meta
+            content={data.description || "Not Found"}
+            name="description"
+          ></meta>
+          <meta content={data.title || "Not Found"} property="og:title" />
+          <meta
+            content={data.description || "Not Found"}
+            property="og:description"
+          />
+          <meta content={data.title || "Not Found"} property="twitter:title" />
+          <meta
+            content={data.description || "Not Found"}
+            property="twitter:description"
+          />
+          <meta property="og:type" content="website" />
+          <meta content="summary_large_image" name="twitter:card" />
+          <meta content="width=device-width, initial-scale=1" name="viewport" />
+          <meta content="Smartjinny" name="generator" />
+        <h1 className="text-center content-center font-extrabold text-6xl">
+          {data.title}
+        </h1>
+        <div className="xl:mx-40 lg:mx-30 sm:mx-20 xs:mx-5 mt-10 prose md:prose-lg lg:prose-xl prose-img:rounded-xl prose-img:max-w-lg prose-a:text-blue-600 hover:prose-a:text-blue-500 max-w-none">
+          <div dangerouslySetInnerHTML={{ __html: data.html }}/>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    retrun (
+      <h1>PAGE NOT FOUND</h1>
+    )
+  }
+  
 }
