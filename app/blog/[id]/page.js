50c1a586d7c0a3213cafba8c29d18bc889aed558
texas000 <texas000@hotmail.com>
2023-02-21 19:04:41 -0500

    typo

--- app/blog/[id]/page.js
+++ app/blog/[id]/page.js
@@ -31,15 +31,15 @@ export default async function Page({ params, searchParams }) {
           <meta content="width=device-width, initial-scale=1" name="viewport" />
           <meta content="Smartjinny" name="generator" />
         <h1 className="text-center content-center font-extrabold text-6xl">
-          {data.title}
+          {data?.title}
         </h1>
         <div className="xl:mx-40 lg:mx-30 sm:mx-20 xs:mx-5 mt-10 prose md:prose-lg lg:prose-xl prose-img:rounded-xl prose-img:max-w-lg prose-a:text-blue-600 hover:prose-a:text-blue-500 max-w-none">
-          <div dangerouslySetInnerHTML={{ __html: data.html }}/>
+          <div dangerouslySetInnerHTML={{ __html: data?.html }}/>
         </div>
       </div>
     );
   } else {
-    retrun (
+    return (
       <h1>PAGE NOT FOUND</h1>
     )
   }
