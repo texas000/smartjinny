ebb08e5acf113ef48dbb30859be7879f28a34661
texas000 <texas000@hotmail.com>
2023-07-23 21:22:17 -0400

    adding gtm

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -30,8 +30,22 @@ export default function Document() {
         `,
           }}
         />
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-T5KW8HH');
+      `}
+        </Script>
       </Head>
       <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T5KW8HH" height="0" width="0" style="display: none; visibility: hidden;" />`,
+          }}
+        />
         <Main />
         <NextScript />
       </body>
